a3818e8dadddb7dcaeb139d2dd5de02573c14b9e
DikaAassila <aassila@dicastalma.com>
2025-05-08 10:35:35 +0100

    Project start

--- coucou/Controllers/HomeController.cs
+++ coucou/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using coucou.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace coucou.Controllers;
 
---
+++ coucou/Models/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace coucou.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+    }
+}
